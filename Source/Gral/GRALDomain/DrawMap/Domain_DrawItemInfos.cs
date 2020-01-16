393510084a24f14cad69acd4b2599d8653b39f78
MarkusKuntner <markus.kuntner@tirol.gv.at>
2020-01-16 19:14:31 +0100

    Add item info as layer

--- Source/Gral/GRALDomain/DrawMap/Domain_DrawItemInfos.cs
+++ Source/Gral/GRALDomain/DrawMap/Domain_DrawItemInfos.cs
@@ -12,9 +12,9 @@
 
 /*
  * Created by SharpDevelop.
- * User: U0178969
- * Date: 01.02.2019
- * Time: 12:53
+ * User: Markus
+ * Date: 16.01.2020
+ * Time: 18:50
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
