1bc3799c28bdaced0096fe32702f87366bec96e0
Markus <markus.kuntner@tirol.gv.at>
2020-10-26 14:32:59 +0100

    Update

--- src/Gral/GRALBackgroundworkers/Background_ReceptorConcentration.cs
+++ src/Gral/GRALBackgroundworkers/Background_ReceptorConcentration.cs
@@ -1,7 +1,7 @@
 #region Copyright
 ///<remarks>
 /// <GRAL Graphical User Interface GUI>
-/// Copyright (C) [2019]  [Dietmar Oettl, Markus Kuntner]
+/// Copyright (C) [2019-2020]  [Dietmar Oettl, Markus Kuntner]
 /// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by
 /// the Free Software Foundation version 3 of the License
 /// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
