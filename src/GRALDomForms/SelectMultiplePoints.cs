6c9faf693774e488ae8eff83b2125762e9a43cce
Markus <markus.kuntner@tirol.gv.at>
2023-10-28 22:55:12 +0200

    Enable editing of existing rows
    Create new lines only when the cursor is at an empty row

--- src/GRALDomForms/SelectMultiplePoints.cs
+++ src/GRALDomForms/SelectMultiplePoints.cs
@@ -148,16 +148,42 @@ namespace GralDomForms
 
         public void ReceiveClickedCoordinates(Object obj, EventArgs e)
         {
-            string _a = "Pt " + Convert.ToString(NameCounter++); 
             if (obj is GralDomain.PointD _pt)
             {
+
+                int row = dataGridView1.CurrentRow.Index;
+                bool existingLine = false;
+                if (row < PointCoorData.Rows.Count)
+                {
+                    if (PointCoorData.Rows[row][0].ToString() != string.Empty)
+                    {
+                        existingLine = true;
+                    }
+                }
+
                 DataRow workrow;
-                workrow = PointCoorData.NewRow();
-                workrow[0] = _a;
+                if (existingLine)
+                {
+                    workrow = PointCoorData.Rows[row];
+                }
+                else
+                {
+                    workrow = PointCoorData.NewRow();
+                    string _a = "Pt " + Convert.ToString(NameCounter++);
+                    workrow[0] = _a;
+                }
+                
                 workrow[1] = _pt.X;
                 workrow[2] = _pt.Y;
-                workrow[3] = 10;
-                PointCoorData.Rows.Add(workrow);
+                if (!existingLine)
+                {
+                    workrow[3] = 10;
+                    PointCoorData.Rows.Add(workrow);
+                }
+                else
+                {
+               
+                }
                 this.Activate();
             }
         }
