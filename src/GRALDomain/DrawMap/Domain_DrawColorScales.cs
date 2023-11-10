9a18a0dba236176aade7f8a8bd74a2ea85d73398
Markus <markus.kuntner@tirol.gv.at>
2023-11-10 17:56:22 +0100

    Bug fix: position of color scales moved, if multiple scales were visible
    A transformation reset is needed; map based color scales are enabled

--- src/GRALDomain/DrawMap/Domain_DrawColorScales.cs
+++ src/GRALDomain/DrawMap/Domain_DrawColorScales.cs
@@ -20,7 +20,7 @@ namespace GralDomain
         /// <summary>
         /// Draw Color Scales
         /// </summary>
-        private void DrawColorScales(Graphics g, float _bmppbx_save)
+        private void DrawColorScales(Graphics g, float _bmppbx_save, double factor_x, double factor_y, double form1_west, double form1_north)
         {
             StringFormat format1 = new StringFormat()  //format for names of sources
             {
@@ -44,91 +44,106 @@ namespace GralDomain
                 {
                     try
                     {
-                        Font font = new Font(_drobj.LabelFont.Name, _drobj.LabelFont.Size);
                         string[] dummy1 = _drobj.ColorScale.Split(new char[] { ',' });
 
                         int xpos1 = 100;
                         int ypos1 = 100;
                         float scale = 1;
+
                         if (dummy1.Length > 2)
                         {
                             scale = Math.Max(0.01F, Convert.ToSingle(dummy1[2].Replace(".", decsep)) / _bmppbx_save);
-                            xpos1 = Convert.ToInt32(Math.Min(32000, Convert.ToDouble(dummy1[0]) / _bmppbx_save / scale));
-                            ypos1 = Convert.ToInt32(Math.Min(32000, Convert.ToDouble(dummy1[1]) / _bmppbx_save / scale));
-                        }
-
-                        g.ScaleTransform(scale, scale);
-
-                        int ydist1 = Math.Max(15, Convert.ToInt32(Math.Min(32000, 170 / _drobj.FillColors.Count)));
-                        
-                        int titleWidth = (int) g.MeasureString(_drobj.LegendTitle, font).Width;
-                        int titleHeight = (int) g.MeasureString(_drobj.LegendTitle, font).Height;
-                        int rectHeight = _drobj.FillColors.Count * ydist1 + titleHeight + 20;
-
-                        SizeF size_of_string = new SizeF(0, 0);
-                        
-                        for (int h = 0; h < _drobj.FillColors.Count; h++)
-                        {
-                            string s = Convert.ToString(Math.Round(_drobj.ItemValues[h], _drobj.DecimalPlaces)) + "  " + _drobj.LegendUnit;
-                            size_of_string = g.MeasureString(s, font);
-                            titleWidth = (int) Math.Max(titleWidth, size_of_string.Width);
-                        }
-                        
-                        int width_rect = titleWidth + 50;
-                        
-                        g.FillRectangle(whiteBrush, xpos1 + 2, ypos1 + 2, width_rect, rectHeight);
-                        g.DrawRectangle(blackPen, xpos1 + 2, ypos1 + 2, width_rect, rectHeight);
-                        g.DrawString(_drobj.LegendTitle, font, blackBrush, (int) (xpos1 + width_rect * 0.5), ypos1 + 5, format1);
-
-                        ypos1 += titleHeight + 15;
-
-                        for (int h = 0; h < _drobj.FillColors.Count; h++)
-                        {
-                            string s = Convert.ToString(Math.Round(_drobj.ItemValues[h], _drobj.DecimalPlaces)) + "  " + _drobj.LegendUnit;
-                            g.DrawString(s, font, blackBrush, xpos1 + width_rect - 4, ypos1 + ydist1 * h, string_Format);
-                        }
-                        
-                        width_rect = Math.Max(10, width_rect - titleWidth - 20);
-                        
-                        int transparency = _drobj.Transparancy;
-                        if (_drobj.DrawSimpleContour == false)
-                        {
-                        	transparency = 255;
-                        }
-                        
-                        for (int h = 0; h < _drobj.FillColors.Count; h++)
-                        {
-                            using (Brush br1 = new SolidBrush(Color2Transparent(transparency, _drobj.FillColors[h]))) // Fill Color
+                            // RemoveSpikes ist used for Map based coordinates, otherwise there are screen based coordinates
+                            if (_drobj.BasedOnMap)
                             {
-                                int x0 = xpos1 + 8;
-                                int y0 = ypos1 + ydist1 * h;
-                                
-//                                if (_drobj.DrawSimpleContour)
-//								  {
-//                                	g.FillRectangle(br1, x0, y0, width_rect, (int) (ydist1 * (_drobj.FillColors.Count - h)/ skalierung1));
-//                                }
-//                                else
-//                                {
-                                	g.FillRectangle(br1, x0, y0, width_rect, ydist1);
-//                                }
-                                
-                                if (h < _drobj.LineColors.Count) // Line Color
+                                scale = (float) (scale / BmpScale);
+                                xpos1 = Convert.ToInt32(((Convert.ToDouble(dummy1[0]) - form1_west) * factor_x + TransformX) / _bmppbx_save / scale);
+                                ypos1 = Convert.ToInt32(((Convert.ToDouble(dummy1[1]) - form1_north) * factor_y + TransformY) / _bmppbx_save / scale);
+                            }
+                            else
+                            {
+                                xpos1 = Convert.ToInt32(Math.Min(32000, Convert.ToDouble(dummy1[0]) / _bmppbx_save / scale));
+                                ypos1 = Convert.ToInt32(Math.Min(32000, Convert.ToDouble(dummy1[1]) / _bmppbx_save / scale));
+                            }
+                        }
+
+                        if (scale > 0.01F)
+                        {
+                            Font font = new Font(_drobj.LabelFont.Name, _drobj.LabelFont.Size);
+
+                            g.ScaleTransform(scale, scale);
+
+                            int ydist1 = Math.Max(15, Convert.ToInt32(Math.Min(32000, 170 / _drobj.FillColors.Count)));
+
+                            int titleWidth = (int)g.MeasureString(_drobj.LegendTitle, font).Width;
+                            int titleHeight = (int)g.MeasureString(_drobj.LegendTitle, font).Height;
+                            int rectHeight = _drobj.FillColors.Count * ydist1 + titleHeight + 20;
+
+                            SizeF size_of_string = new SizeF(0, 0);
+
+                            for (int h = 0; h < _drobj.FillColors.Count; h++)
+                            {
+                                string s = Convert.ToString(Math.Round(_drobj.ItemValues[h], _drobj.DecimalPlaces)) + "  " + _drobj.LegendUnit;
+                                size_of_string = g.MeasureString(s, font);
+                                titleWidth = (int)Math.Max(titleWidth, size_of_string.Width);
+                            }
+
+                            int width_rect = titleWidth + 50;
+
+                            g.FillRectangle(whiteBrush, xpos1 + 2, ypos1 + 2, width_rect, rectHeight);
+                            g.DrawRectangle(blackPen, xpos1 + 2, ypos1 + 2, width_rect, rectHeight);
+                            g.DrawString(_drobj.LegendTitle, font, blackBrush, (int)(xpos1 + width_rect * 0.5), ypos1 + 5, format1);
+
+                            ypos1 += titleHeight + 15;
+
+                            for (int h = 0; h < _drobj.FillColors.Count; h++)
+                            {
+                                string s = Convert.ToString(Math.Round(_drobj.ItemValues[h], _drobj.DecimalPlaces)) + "  " + _drobj.LegendUnit;
+                                g.DrawString(s, font, blackBrush, xpos1 + width_rect - 4, ypos1 + ydist1 * h, string_Format);
+                            }
+
+                            width_rect = Math.Max(10, width_rect - titleWidth - 20);
+
+                            int transparency = _drobj.Transparancy;
+                            if (_drobj.DrawSimpleContour == false)
+                            {
+                                transparency = 255;
+                            }
+
+                            for (int h = 0; h < _drobj.FillColors.Count; h++)
+                            {
+                                using (Brush br1 = new SolidBrush(Color2Transparent(transparency, _drobj.FillColors[h]))) // Fill Color
                                 {
-                                    using (Pen pen1 = new Pen(_drobj.LineColors[h], 3))
+                                    int x0 = xpos1 + 8;
+                                    int y0 = ypos1 + ydist1 * h;
+
+                                    //                                if (_drobj.DrawSimpleContour)
+                                    //								  {
+                                    //                                	g.FillRectangle(br1, x0, y0, width_rect, (int) (ydist1 * (_drobj.FillColors.Count - h)/ skalierung1));
+                                    //                                }
+                                    //                                else
+                                    //                                {
+                                    g.FillRectangle(br1, x0, y0, width_rect, ydist1);
+                                    //                                }
+
+                                    if (h < _drobj.LineColors.Count) // Line Color
                                     {
-                                        g.DrawLine(pen1, x0, y0, x0 + width_rect + 7, y0);
+                                        using (Pen pen1 = new Pen(_drobj.LineColors[h], 3))
+                                        {
+                                            g.DrawLine(pen1, x0, y0, x0 + width_rect + 7, y0);
+                                        }
                                     }
                                 }
                             }
+                            font.Dispose();
+                            g.ResetTransform();
                         }
-                        font.Dispose();
                     }
                     catch
                     {
                     }
                 }
             }
-            g.ResetTransform();
             blackBrush.Dispose();
             whiteBrush.Dispose();
             blackPen.Dispose();
