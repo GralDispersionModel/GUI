fe37acc5c74ea193e6a8f3dc294f228a92fcf736
Markus <markus.kuntner@tirol.gv.at>
2025-11-20 18:08:06 +0100

    New form when starting a GRAL calculation with a chunk of situations

---
+++ src/GRALMainSubForms/StartandFinalSituation.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Gral.GralMainForms
+{
+    partial class StartandFinalSituation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(StartandFinalSituation));
+            numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            button3 = new System.Windows.Forms.Button();
+            button2 = new System.Windows.Forms.Button();
+            label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            numericUpDown1.Location = new System.Drawing.Point(163, 45);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new System.Drawing.Size(102, 25);
+            numericUpDown1.TabIndex = 0;
+            numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            numericUpDown2.Location = new System.Drawing.Point(163, 86);
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new System.Drawing.Size(102, 25);
+            numericUpDown2.TabIndex = 1;
+            numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            label1.Location = new System.Drawing.Point(29, 45);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(85, 17);
+            label1.TabIndex = 2;
+            label1.Text = "First situation";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            label2.Location = new System.Drawing.Point(29, 88);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(87, 17);
+            label2.TabIndex = 3;
+            label2.Text = "Final situation";
+            // 
+            // button3
+            // 
+            button3.DialogResult = System.Windows.Forms.DialogResult.OK;
+            button3.Location = new System.Drawing.Point(167, 145);
+            button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            button3.Name = "button3";
+            button3.Size = new System.Drawing.Size(98, 37);
+            button3.TabIndex = 4;
+            button3.Text = "&OK";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button2
+            // 
+            button2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            button2.Location = new System.Drawing.Point(29, 145);
+            button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            button2.Name = "button2";
+            button2.Size = new System.Drawing.Size(98, 37);
+            button2.TabIndex = 5;
+            button2.Text = "&Cancel";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0);
+            label3.Location = new System.Drawing.Point(17, 15);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(163, 17);
+            label3.TabIndex = 6;
+            label3.Text = "Start one instance of GRAL";
+            // 
+            // StartandFinalSituation
+            // 
+            AcceptButton = button3;
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            CancelButton = button2;
+            ClientSize = new System.Drawing.Size(297, 206);
+            Controls.Add(label3);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(numericUpDown2);
+            Controls.Add(numericUpDown1);
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            Icon = (System.Drawing.Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "StartandFinalSituation";
+            Text = "Set first and filnal situation";
+            TopMost = true;
+            Load += StartandFinalSituation_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+    }
+}
---
+++ src/GRALMainSubForms/StartandFinalSituation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Gral.GralMainForms
+{
+    public partial class StartandFinalSituation : Form
+    {
+        private int _maxSituation;
+        public int StartSituation;
+        public int FinalSituation;
+
+        public StartandFinalSituation(int MaxSituation)
+        {
+            _maxSituation = MaxSituation;
+
+            InitializeComponent();
+        }
+
+        public void StartandFinalSituation_Load(object sender, EventArgs e)
+        {
+            numericUpDown1.Minimum = 1;
+            numericUpDown1.Maximum = _maxSituation;
+            numericUpDown1.Value = 1;
+            numericUpDown2.Minimum = 1;
+            numericUpDown2.Maximum = _maxSituation;
+            numericUpDown2.Value = _maxSituation;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StartSituation = (int) numericUpDown1.Value;
+            FinalSituation = (int) numericUpDown2.Value;
+        }
+    }
+}
